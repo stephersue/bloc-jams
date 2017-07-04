6733235c0b7db3d544ef75d4b2b44961c951abf2
Stephanie Strong <stephaniestrong@Stephanies-MacBook-Pro.local>
2017-07-04 16:14:54 -0600

    Assignment -9-events: Not Complete, try.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,21 +1,19 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
-
-    var revealPoint = function() {
-      for(var i=0; i < points.length; i++){
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-      }
-    };
-    revealPoint();
-};
+function forEach(points, function revealPoint() {
+    for(var i=0; i < points.length; i++){
+      points[i].style.opacity = 1;
+      points[i].style.transform = "scaleX(1) translateY(0)";
+      points[i].style.msTransform = "scaleX(1) translateY(0)";
+      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+  };
+  revealPoint();
+});
 
 window.onload = function() {
   if (window.innerHeight > 950) {
-    animatePoints(pointsArray);
+    forEach(pointsArray, revealPoint);
   }
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
